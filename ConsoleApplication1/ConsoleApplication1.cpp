e22e3ac6565c9fec7785cc3295be4899ef012f25
timge <timge@ASPIREV15NITRO>
2017-11-02 17:17:21 +0100

    Feierabend Donnerstag Tim

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -29,14 +29,15 @@ class Huhn
 {
 public:
 	bool Sichtbarkeit, Wandern, von_links_wandern;
-	double XPos, YPos, ZEbene, Winkel, PfAwX, PfAwY, SkalX, SkalY, Punkte, Höhe, Breite, start_wandern, Faktor_Bewegung;
+	double XPos, YPos, ZEbene, Winkel, PfAwX, PfAwY, SkalX, SkalY,  Höhe, Breite, start_wandern, Faktor_Bewegung, Punkte;
+	
 	string JPG;
 	Gosu::Image Bild;
 
 	void draw()
 	{
-		if (this->Wandern && this->Sichtbarkeit && this->von_links_wandern) { this->XPos = this->XPos + 1 * this->Faktor_Bewegung; }
-		if (this->Wandern && this->Sichtbarkeit & !this->von_links_wandern) { this->XPos = this->XPos - 1 * this->Faktor_Bewegung; }
+		if (this->Wandern && this->Sichtbarkeit && this->von_links_wandern) { this->XPos = this->XPos +( 1 * this->Faktor_Bewegung); }
+		if (this->Wandern && this->Sichtbarkeit && !(this->von_links_wandern)) { this->XPos = this->XPos -( 1 * this->Faktor_Bewegung); }
 		//else if (this->Wandern && (this->XPos >= 1920+ this->Breite))	{this->XPos = this->start_wandern;}
 		if (this->XPos + this->Breite > 1920) { this->Sichtbarkeit = false; this->XPos = this->start_wandern; }
 
@@ -60,7 +61,7 @@ public:
 
 	}
 
-	double getroffen(double x, double y)
+	int getroffen(double x, double y)
 		// Die Funktion später nur ausführen wenn ein gültiger Klick gemacht wurde
 	{
 
@@ -70,15 +71,15 @@ public:
 		if (((x >= this->XPos) && (x <= (this->XPos + Breite))) &&
 			((y >= this->YPos) && (y <= (this->YPos + Höhe))) && this->Sichtbarkeit)
 		{
+			cout << "XPos: " << this->XPos << endl;
+			cout << "YPos: " << this->YPos << endl;
+			cout << "Breite: " << this->Breite << endl;
+			cout << "Höhe: " << this->Höhe << endl;
 
 			this->ZEbene = -7;
 			Sichtbarkeit = false;
-			if (Wandern)
-			{
-				this->XPos = this->start_wandern;
-
-			}
-
+			if (this->Wandern && this->von_links_wandern)	{this->XPos = - this->Breite;}
+			if (this->Wandern && !this->von_links_wandern) { this->XPos = 1920 + this->Breite; }
 
 			return this->Punkte;
 
@@ -95,9 +96,14 @@ public:
 
 
 	{
+		this->SkalX = skalx;
+		this->SkalY = skaly;
 
-		this->Höhe = this->Bild.height()* this->SkalX;
-		this->Breite = this->Bild.height()*this->SkalY;
+		this->Höhe = this->Bild.height() * this->SkalY;
+		this->Breite = this->Bild.width() * this->SkalX;
+
+		cout << "Höhe: " << this->Höhe << endl;
+		cout << "Breite: " << this->Breite << endl;
 
 		this->Sichtbarkeit = sichtbarkeit;
 		this->Wandern = wandern;
@@ -109,13 +115,14 @@ public:
 		this->Winkel = winkel;
 		this->PfAwX = pfawx;
 		this->PfAwY = pfawy;
-		this->SkalX = skalx;
-		this->SkalY = skaly;
+
 		this->Punkte = punkte;
 		this->JPG = jpg;
 
-		this->Höhe = this->Bild.height()* this->SkalX;
-		this->Breite = this->Bild.height()*this->SkalY;
+		if (this->Wandern && this->von_links_wandern) { this->XPos = - this->Breite; }
+		if (this->Wandern && !this->von_links_wandern) { this->XPos = 1920 +this->Breite; }
+
+
 
 		// #tim muss noch unterscheidung rein, wenns von rechts wandern soll muss es anderen Startwert haben...
 		if (this->Wandern)
@@ -137,17 +144,17 @@ public:
 	Gosu::Image Schuss;
 	Gosu::Sample shot;
 	Gosu::Sample reload;
-	Gosu::Image ImgH1;
-	Gosu::Image ImgH2;
-	Gosu::Font punkte;
+	//Gosu::Image ImgH1;
+	//Gosu::Image ImgH2;
+	Gosu::Font Punkte_Anzeige;
 	Gosu::Font ueberschrift;
 
 
 
 	GameWindow()
 		:Window(1920, 1200)
-		, background("Back.jpg"), Schuss("Schuss.png"), ImgH1("H1.jpg"), ImgH2("H1.jpg"), shot("shot.wav"), reload("reload.wav"),
-		punkte(80), ueberschrift(40)
+		, background("Back.jpg"), Schuss("Schuss.png"), /*ImgH1("H1.jpg"), ImgH2("H1.jpg"),*/ shot("shot.wav"), reload("reload.wav"),
+		Punkte_Anzeige(80), ueberschrift(40)
 
 	{
 		set_caption("Gosu Tutorial Game");
@@ -158,17 +165,20 @@ public:
 		double pfawx, double pfawy, double skalx, double skaly, double punkte, string jpg)
 		: Bild(jpg)*/
 
-		Huhn H01(false, false, true, 1, 120, 450, -7, 0, 0, 0, 0.2, 0.2, 25, "H1.jpg");		//Auf Heuballen
-		Huhn H02(false, false, true, 1, 245, 620, -7, 0, 0, 0, 0.2, 0.2, 25, "H1.jpg");		//Rechts neben Heuballen
-		Huhn H03(false, false, true, 1, 65, 281, -7, 0, 0, 0, 0.2, 0.2, 25, "H1.jpg");		//ganz links Scheunendach
-		Huhn H04(false, false, true, 1, 765, 75, -7, 0, 0, 0, 0.2, 0.2, 25, "H1.jpg");		//Eier auf Strommasten
-		Huhn H05(false, false, true, 1, 910, 155, -7, 0, 0, 0, 0.2, 0.2, 25, "H1.jpg");		//Schornstein Mitte
-		Huhn H06(false, false, true, 1, 1600, 130, -7, 0, 0, 0, 0.2, 0.2, 25, "H1.jpg");		//Schornstein rechts
-		Huhn H07(false, false, true, 1, 990, 410, -7, 0, 0, 0, 0.2, 0.2, 25, "H1.jpg");		//Zaun links
-		Huhn H08(false, false, true, 1, 1360, 400, -7, 0, 0, 0, 0.2, 0.2, 25, "H1.jpg");		//Zaun mitte
-		Huhn H09(false, false, true, 1, 1780, 450, -7, 0, 0, 0, 0.2, 0.2, 25, "H1.jpg");		//Zaun rechts
+		Huhn H01(false, false, true, 1, 120, 450, -7, 0, 0, 0, 0.2, 0.2, 25, "MH_Zeitung.png");		//Auf Heuballen
+		Huhn H02(false, false, true, 1, 245, 620, -7, 0, 0, 0, 0.5, 0.5, 25, "MH_Gangsta.png");		//Rechts neben Heuballen
+		Huhn H03(false, false, true, 1, 65, 281, -7, 0, 0, 0, 0.2, 0.2, 25, "MH_Zeitung.png");		//ganz links Scheunendach
+		Huhn H04(false, false, true, 1, 765, 75, -7, 0, 0, 0, 0.2, 0.2, 25, "MH_Zeitung.png");		//Eier auf Strommasten
+		Huhn H05(false, false, true, 1, 910, 155, -7, 0, 0, 0, 0.2, 0.2, 25, "MH_Zeitung.png");		//Schornstein Mitte
+		Huhn H06(false, false, true, 1, 1600, 130, -7, 0, 0, 0, 0.2, 0.2, 25, "MH_Zeitung.png");		//Schornstein rechts
+		Huhn H07(false, false, true, 1, 990, 410, -7, 0, 0, 0, 0.2, 0.2, 25, "MH_Zeitung.png");		//Zaun links
+		Huhn H08(false, false, true, 1, 1360, 400, -7, 0, 0, 0, 0.2, 0.2, 25, "MH_Zeitung.png");		//Zaun mitte
+		Huhn H09(false, false, true, 1, 1780, 450, -7, 0, 0, 0, 0.2, 0.2, 25, "MH_Zeitung.png");		//Zaun rechts
+		Huhn H10(false, false, true, 1, 800, 600, -7, 0, 0, 0, 1, 1, 1, "MH_Gangsta.png");		//Rechts neben Heuballen*/
 
-		Huhn H50(false, true, true, 1, 500, 500, -7, 0, 0, 0, 0.2, 0.2, 25, "H1.jpg");		//fliegende Hühner
+		Huhn H50(false, true, true, 3, 0, 50, -7, 0, 0, 0, 0.1, 0.1, 100, "MH_fliegt_vonlinks.png");		//fliegende Hühner
+		Huhn H51(false, true, true, 2, 0, 250, -7, 0, 0, 0, 0.2, 0.2, 25, "MH_fliegt_vonlinks.png");		//fliegende Hühner
+		Huhn H52(false, true, true, 5, 0, 500, -7, 0, 0, 0, 0.3, 0.3, 5, "MH_fliegt_vonlinks.png");		//fliegende Hühner
 
 		Huehner.push_back(H01);
 		Huehner.push_back(H02);
@@ -179,16 +189,22 @@ public:
 		Huehner.push_back(H07);
 		Huehner.push_back(H08);
 		Huehner.push_back(H09);
+		Huehner.push_back(H10);
 
-
+		Huehner.push_back(H50);
+		Huehner.push_back(H51);
+		Huehner.push_back(H52);
 
 	}
 
 	double x, y, Punktestand = 0;
 	bool Klick_links, Klick_links_alt, Klick_rechts, Klick_rechts_alt, schiesen, laden;
 	bool H1 = true, H2 = true, H3 = true, H4, H5, H6, H7, H8, H9, H10;
-	int Schüsse = 5, Zufallszahl_von = 1, Zufallszahl_bis = 9, Zufallszahl, Zi = 0;
+	int Schüsse = 5, Zufallszahl_von = 1, Zufallszahl_bis = 13, Zufallszahl, Zi = 0;
+	string string_Punktestand;
 	vector<Huhn> Huehner;
+	
+
 
 
 
@@ -218,13 +234,12 @@ public:
 
 						for (Huhn& elem : Huehner)
 						{
-							//cout << elem.XPos;
+							
 
 							Punktestand = elem.getroffen(x, y) + Punktestand/*+ Punktestand*/;
-							cout << Punktestand << endl;
+							
 						}
-					//cout << endl;
-					//cout << Huehner[0].ZEbene << endl;
+					
 
 				}
 				else if (!Klick_links)
@@ -248,61 +263,28 @@ public:
 		}
 
 
-
-		if (Zi<100)
+		// Hühner wieder auftauchen lassen
 		{
-			Zi++;
-			//cout << "     " << i << endl;
+			if (Zi < 50)
+			{
+				Zi++;
+				
+			}
+			else
+			{
+				Zufallszahl = (rand() % ((Zufallszahl_bis + 1) - Zufallszahl_von)) + Zufallszahl_von;
+				Huehner[Zufallszahl - 1].ZEbene = 7;
+				Huehner[Zufallszahl - 1].Sichtbarkeit = true;
+				
+				Zi = 0;
+			}
 		}
-		else
-		{
-			Zufallszahl = (rand() % ((Zufallszahl_bis + 1) - Zufallszahl_von)) + Zufallszahl_von;
-			Huehner[Zufallszahl - 1].ZEbene = 7;
-			Huehner[Zufallszahl - 1].Sichtbarkeit = true;
-			//cout << Zufallszahl << endl;
-			Zi = 0;
-		}
-
-		/*
-		#include <stdio.h>
-		#include <stdlib.h>
-		#include <time.h>
-
-		int
-		main ()
-		{
-		const int x = 4, y = 10;
-		int random;
-
-		srand (time (NULL));
-
-		/* erzeugt Zufallszahlen zwischen 4 und 10 inklusive
-		random = (rand() % ((y + 1) - x)) + x;
-		printf("%d\n", random);
-
-		return 0;
-		}
-		*/
-
-
-
-
-
-
-
-
-		/*if (schiesen) // Wird nur durchlaufen wenn ein gültiger Schuss abgegeben wurde
-		for (Huhn elem : Huehner)
-		{
-		Punktestand = elem.getroffen(x, y) + Punktestand;
-		}*/
-
-		//cout << Punktestand << endl;
+		
 
 
 		//Klicke rechte Maustaste
 		{
-			Klick_rechts = input().down(Gosu::MS_RIGHT);
+			Klick_rechts = input().down(Gosu::KB_SPACE) || input().down(Gosu::MS_RIGHT);
 			if (Klick_rechts && !Klick_rechts_alt)
 			{
 				Klick_rechts_alt = true;
@@ -325,6 +307,8 @@ public:
 
 		//
 	}
+
+
 	void draw() override {
 
 
@@ -337,8 +321,11 @@ public:
 		//           AlphaMode  mode= AM_DEFAULT)
 		//Draws text, so the top left corner of the text is at (x,y)
 
+
+		string_Punktestand = to_string(int(Punktestand) );	
+
 		ueberschrift.draw("Punkte", 1542, 0, 9, 1, 1, Gosu::Color::BLACK);
-		punkte.draw("1050", 1520, 24, 9, 1, 1, Gosu::Color::BLACK);
+		Punkte_Anzeige.draw(string_Punktestand, 1520, 24, 9, 1, 1, Gosu::Color::BLACK);
 
 
 
